244c2821edb4d9dc7ebcecb12db6190f464a79af
djib207 <118479361+djib207@users.noreply.github.com>
2022-11-19 11:02:53 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+#problem3_7("flowers.csv","alyssum")
+
+def problem1_2(x,y):
+    print (x+y)
+    print (x*y)
+
+
+num1=6
+num2=6
+abc=problem1_2(num1,num2)
+print("result:",abc)
+
+
+def problem1_3(n):
+    my_sum = 0
+    for number in range(n+1):
+        my_sum = my_sum + number
+        print (number,my_sum,)
+
+problem1_3(4)
